c70536cc26643b588768e6d3b027454d22815906
sanchit237 <sanchitnemlekar8@gmail.com>
2022-04-15 12:35:36 +0530

    Email functionality added

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -31,8 +31,18 @@ class Register extends BaseController
                 $result = $this->RegisterModel->registerdata($registerdata);
                 
                 if ($result){
-                    $this->session->setFlashdata('reg_success', 'Account created successfully');
-                    return redirect()->to(current_url());
+                    $this->email->setFrom('gamingsn237@gmail.com', 'sanchit');
+                    $this->email->setTo($this->request->getPost('reg_email'));
+                    $this->email->setSubject('Email Test');
+                    $this->email->setMessage('Testing the email class.');
+                    if ($this->email->send()){
+                        $this->session->setFlashdata('reg_success', 'Account created successfully');
+                        return redirect()->to(current_url());
+                    }
+                    else {
+                        $this->session->setFlashdata('reg_failure', 'Error in Account creation');
+                        return redirect()->to(current_url());
+                    }
                 }
                 else {
                     $this->session->setFlashdata('reg_failure', 'Error in Account creation');
